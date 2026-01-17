b16581f1fc04bbd2e1523be0a7f41f108023b666
Abdullah <abdullahbelal102938@gmail.com>
2026-01-17 16:49:08 +0200

    kk

--- frontend/base.ts
+++ frontend/base.ts
@@ -1,1 +1,2 @@
-export const NEXT_PUBLIC_API_URL = `http://localhost:3001`
+// export const NEXT_PUBLIC_API_URL = `http://localhost:3001`
+export const NEXT_PUBLIC_API_URL = `https://al7ram-api.nabdtech.store`
